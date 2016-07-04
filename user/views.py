2a968e6c92d2b58aaf09c76830d9fbcc9dc4ca27
Ivelina Hristova <iv4etoxx@gmail.com>
2016-07-05 01:14:13 +0300

    user profile

--- user/views.py
+++ user/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.contrib.auth import authenticate, login, logout
 
+from foods.models import ConsumedProducts
+from foods.models import Diet
+from foods.models import Foods_Nutrients
+
 def sign_in(request):
 
     username = request.POST.get('username', False)
@@ -32,3 +36,32 @@ def sign_in(request):
 def sign_out(request):
     logout(request)
     return HttpResponseRedirect('/foods/')
+
+def profile(request):
+    user_id = request.user.id
+    diet_elements = Diet.objects.filter(user__id = user_id)
+    consumed_nutrients = dict()    
+   
+    consumed_foods = ConsumedProducts.objects.filter(user__id = user_id)
+    for consumed_food in consumed_foods:
+        nutrients = Foods_Nutrients.objects.filter(food__food_id = consumed_food.id)
+        # return HttpResponse(diet_element.nutrient.nutrient_id)
+        for nutrient in nutrients:
+            if nutrient.nutrient_id in consumed_nutrients.keys():
+                consumed_nutrients[nutrient.nutrient_id] += nutrient.grams * consumed_food.count
+            else:
+                consumed_nutrients[nutrient.nutrient_id] = nutrient.grams * consumed_food.count
+
+    nutrients = dict()
+    for diet_element in diet_elements:
+        nutrients[diet_element.nutrient.nutrient_id] = [
+        consumed_nutrients[diet_element.nutrient.nutrient_id] * 100 / diet_element.grams,
+        consumed_nutrients[diet_element.nutrient.nutrient_id],
+        diet_element.grams
+        ]
+
+    template = loader.get_template('user/profile.html')
+    context = {
+        'nutrients': nutrients,
+    }
+    return HttpResponse(template.render(context, request))
